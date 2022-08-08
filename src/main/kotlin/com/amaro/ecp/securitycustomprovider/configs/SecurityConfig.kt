6f561733df9ea3361f0764d17e05aff0b2b4166d
joao-soares-amaro <84871410+joao-soares-amaro@users.noreply.github.com>
2022-08-08 12:34:39 -0300

    feat: reading groups from JWT improved

--- src/main/kotlin/com/amaro/ecp/securitycustomprovider/configs/SecurityConfig.kt
+++ src/main/kotlin/com/amaro/ecp/securitycustomprovider/configs/SecurityConfig.kt
@@ -7,7 +7,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.web.SecurityFilterChain
+import java.util.*
 
 
 @EnableWebSecurity
@@ -18,33 +21,40 @@ data class SecurityConfig(
 ) {
 
     @Bean
-    fun filterChain(http: HttpSecurity, extractor: GrantedAuthoritiesExtractor): SecurityFilterChain {
-        http.csrf().disable().requestMatchers { getAntMatchers(it) }.authorizeHttpRequests { getAntMatchers(it) }
-            .oauth2ResourceServer().jwt().jwtAuthenticationConverter(extractor)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+            .requestMatchers { getAntMatchers(it) }
+            .authorizeHttpRequests { getAntMatchers(it) }
+            .oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(grantedAuthoritiesExtractor())
         return http.build()
+    }
+
+    private fun grantedAuthoritiesExtractor(): JwtAuthenticationConverter {
+        val jwtAuthenticationConverter = JwtAuthenticationConverter()
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter { jwt ->
+            val list: List<String> = jwt.claims.getOrDefault("cognito:groups", emptyList<String>()) as List<String>
+            return@setJwtGrantedAuthoritiesConverter list
+                .map { obj: Any -> obj.toString() }
+                .map { role -> SimpleGrantedAuthority("ROLE_${role.uppercase(Locale.getDefault())}") }
+        }
+        return jwtAuthenticationConverter
     }
 
     private fun getAntMatchers(requestMatcherConfigurer: HttpSecurity.RequestMatcherConfigurer): HttpSecurity.RequestMatcherConfigurer {
         secure.forEach {
             if (it.methods.contains("*")) requestMatcherConfigurer.antMatchers(it.path)
-            else it.methods.forEach { method ->
-                requestMatcherConfigurer.antMatchers(
-                    HttpMethod.valueOf(method), it.path
-                )
-            }
+            else it.methods.forEach { method -> requestMatcherConfigurer.antMatchers(HttpMethod.valueOf(method), it.path) }
         }
         return requestMatcherConfigurer
     }
 
     private fun getAntMatchers(authorizeHttpRequestsConfigurer: AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry): AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry {
         secure.forEach {
-            if (it.methods.contains("*")) authorizeHttpRequestsConfigurer.antMatchers(it.path)
-                .hasAnyRole(*it.roles.toTypedArray())
-            else it.methods.forEach { method ->
-                authorizeHttpRequestsConfigurer.antMatchers(
-                    HttpMethod.valueOf(method), it.path
-                ).hasAnyRole(*it.roles.toTypedArray())
-            }
+            if (it.methods.contains("*")) authorizeHttpRequestsConfigurer.antMatchers(it.path).hasAnyRole(*it.roles.toTypedArray())
+            else it.methods.forEach { method -> authorizeHttpRequestsConfigurer.antMatchers(HttpMethod.valueOf(method), it.path).hasAnyRole(*it.roles.toTypedArray()) }
         }
         return authorizeHttpRequestsConfigurer
     }
